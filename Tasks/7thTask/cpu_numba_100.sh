35135629acd0b39503b177d42dcff58aea2c7274
Levente Buzga <levente@tactile.dk>
2025-04-30 08:06:49 +0200

    add content to Tasks folder

---
+++ Tasks/7thTask/cpu_numba_100.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#BSUB -R "rusage[mem=1024MB] select[model==XeonGold6126] span[hosts=1]"
+#BSUB -q hpc
+#BSUB -W 60
+#BSUB -J cpu_numba
+#BSUB -n 1
+#BSUB -o cpu_numba_100%J.out
+#BSUB -e cpu_numba_100%J.err
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+time python sim.py 100
